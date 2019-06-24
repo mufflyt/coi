e09c6387c7dae09689c4464b3aa9ecb4c678af3a
Tyler Muffly <tyler.muffly@dhha.org>
2019-06-24 13:12:07 -0600

    Changed to work on my machine at home.

--- addNPI R2.R
+++ addNPI R2.R
@@ -3,35 +3,51 @@ install.packages("readr")
 install.packages("qdapRegex")
 install.packages("sqldf")
 # Loading
-library("sqldf", lib.loc="~/R/win-library/3.2")
-library("qdapRegex", lib.loc="~/R/win-library/3.2")
-library("sqldf", lib.loc="~/R/win-library/3.2")
+library("sqldf")
+library("qdapRegex")
+library("sqldf")
 library("readr")
+library("tidyverse")
+library("dplyr")
+library("inspectdf")
+library("Hmisc")
+library("DataExplorer")
+library("GGally")
 
 #Start
+#Start
+OBGYN_List <- (c("Allopathic & Osteopathic Physicians|Obstetrics & Gynecology", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Critical Care Medicine", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Female Pelvic Medicine and Reconstructive Surgery", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Gynecologic Oncology", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Gynecology", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Hospice and Palliative Medicine", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Maternal & Fetal Medicine", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Obesity Medicine", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Obstetrics", "Allopathic & Osteopathic Physicians|Obstetrics & Gynecology|Reproductive Endocrinology")) %>% 
+  as.data.frame()
+"Physician_Specialty" -> names(OBGYN_List)
+colnames(OBGYN_List)
 
-OBGYN_List <- read.csv("D:/muffly/Guido/OP_OBGYN.csv", stringsAsFactors = FALSE)
 
-OP_13 <- read.csv("D:/muffly/data/Originals/PGYR13_P062918/OP_DTL_GNRL_PGYR2013_P06292018.csv",stringsAsFactors = FALSE)
+OP_13 <- read.csv("~/Dropbox/Pharma_Influence/data/Open Payments/OP_DTL_GNRL_PGYR2013_P06292018.csv",stringsAsFactors = FALSE)
 OP_13 <- sqldf('select OP_13.* from OP_13 join OBGYN_List on OP_13.Physician_Specialty = OBGYN_List.Physician_Specialty' )
 OP_13 <- OP_13[OP_13$Name_of_Associated_Covered_Drug_or_Biological1 !="",]
 
-OP_14 <- read.csv("D:/muffly/data/Originals/PGYR14_P062918/OP_DTL_GNRL_PGYR2014_P06292018.csv",stringsAsFactors = FALSE)
+OP_14 <- read.csv("~/Dropbox/Pharma_Influence/data/Open Payments/OP_DTL_GNRL_PGYR2014_P06292018.csv",stringsAsFactors = FALSE)
 OP_14<- sqldf('select OP_14.* from OP_14 join OBGYN_List on OP_14.Physician_Specialty = OBGYN_List.Physician_Specialty' )
 OP_14 <- OP_14[OP_14$Name_of_Associated_Covered_Drug_or_Biological1 !="",]
 
-OP_15 <- read.csv("D:/muffly/data/Originals/PGYR15_P062918/OP_DTL_GNRL_PGYR2015_P06292018.csv",stringsAsFactors = FALSE)
+OP_15 <- read.csv("~/Dropbox/Pharma_Influence/data/Open Payments/OP_DTL_GNRL_PGYR2015_P06292018.csv",stringsAsFactors = FALSE)
 OP_15<- sqldf('select OP_15.* from OP_15 join OBGYN_List on OP_15.Physician_Specialty = OBGYN_List.Physician_Specialty' )
 OP_15 <- OP_15[OP_15$Name_of_Associated_Covered_Drug_or_Biological1 !="",]
 
-OP_16 <- read.csv("D:/muffly/data/Originals/PGYR16_P062918/OP_DTL_GNRL_PGYR2016_P06292018.csv",stringsAsFactors = FALSE)
+OP_16 <- read.csv("~/Dropbox/Pharma_Influence/data/Open Payments/OP_DTL_GNRL_PGYR2016_P06292018.csv",stringsAsFactors = FALSE)
 OP_16<- sqldf('select OP_16.* from OP_16 join OBGYN_List on OP_16.Physician_Specialty = OBGYN_List.Physician_Specialty' )
 OP_16 <- OP_16[OP_16$Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1 =="Drug" | OP_16$Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1 =="Biological" ,]
 
-OP_17 <- read.csv("D:/muffly/data/Originals/PGYR17_P062918/OP_DTL_GNRL_PGYR2017_P06292018.csv",stringsAsFactors = FALSE)
+OP_17 <- read.csv("~/Dropbox/Pharma_Influence/data/Open Payments/OP_DTL_GNRL_PGYR2017_P06292018.csv",stringsAsFactors = FALSE)
 OP_17<- sqldf('select OP_17.* from OP_17 join OBGYN_List on OP_17.Physician_Specialty = OBGYN_List.Physician_Specialty' )
 OP_17 <- OP_17[OP_17$Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1 =="Drug" | OP_17$Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1 =="Biological",]
-  
+
+cols_15 <- colnames(OP_15) %>% as.data.frame()
+cols_16 <- colnames(OP_16) %>% as.data.frame()
+outer <- dplyr::anti_join(cols_15, cols_16)
+outer
+outer2 <- dplyr::anti_join(cols_16, cols_15)
+outer2
 
 # reorder 16 and 17
 OP_16 <- OP_16[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,52,57,62,67,72,53,58,63,68,73,52,57,62,67,72,74,75,49,50,51,54,55,56,59,60,61,64,65,66,69,70,71)]
@@ -53,6 +69,8 @@ OP_16 <- OP_16[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
 # reorder 16 and 17
 
 OP_17 <- OP_17[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,52,57,62,67,72,53,58,63,68,73,52,57,62,67,72,74,75,49,50,51,54,55,56,59,60,61,64,65,66,69,70,71)]
+
+
 
 names(OP_17)[c(49, 50, 51, 52, 53)] <- c("Name_of_Associated_Covered_Drug_or_Biological1","Name_of_Associated_Covered_Drug_or_Biological2", "Name_of_Associated_Covered_Drug_or_Biological3","Name_of_Associated_Covered_Drug_or_Biological4","Name_of_Associated_Covered_Drug_or_Biological5")
 
@@ -79,19 +97,19 @@ rm(OP_15)
 rm(OP_16)
 rm(OP_17)
 
-Prescriber_13 <- read.delim("D:/muffly/data/Originals/PartD_Prescriber_PUF_NPI_13/PartD_Prescriber_PUF_NPI_13.txt", stringsAsFactors=FALSE)
+Prescriber_13 <- read.delim("~/Dropbox/Pharma_Influence/data/Medicare Part D data/PartD_Prescriber_PUF_NPI_13/PartD_Prescriber_PUF_NPI_13.txt", stringsAsFactors=FALSE)
 Prescriber_13 <- Prescriber_13[Prescriber_13$specialty_description == "Obstetrics/Gynecology",]
 
-Prescriber_14 <- read.delim("D:/muffly/data/Originals/PartD_Prescriber_PUF_NPI_14/PartD_Prescriber_PUF_NPI_14.txt", stringsAsFactors=FALSE)
+Prescriber_14 <- read.delim("~/Dropbox/Pharma_Influence/data/Medicare Part D data/PartD_Prescriber_PUF_NPI_14/PartD_Prescriber_PUF_NPI_14.txt", stringsAsFactors=FALSE)
 Prescriber_14 <- Prescriber_14[Prescriber_14$specialty_description == "Obstetrics/Gynecology",]
 
-Prescriber_15 <- read.delim("D:/muffly/data/Originals/PartD_Prescriber_PUF_NPI_15/PartD_Prescriber_PUF_NPI_15.txt", stringsAsFactors=FALSE)
+Prescriber_15 <- read.delim("~/Dropbox/Pharma_Influence/data/Medicare Part D data/PartD_Prescriber_PUF_NPI_15/PartD_Prescriber_PUF_NPI_15.txt", stringsAsFactors=FALSE)
 Prescriber_15 <- Prescriber_15[Prescriber_15$specialty_description == "Obstetrics/Gynecology",]
 
-Prescriber_16 <- read.delim("D:/muffly/data/Originals/PartD_Prescriber_PUF_NPI_16/PartD_Prescriber_PUF_NPI_16.txt", stringsAsFactors=FALSE)
+Prescriber_16 <- read.delim("~/Dropbox/Pharma_Influence/data/Medicare Part D data/PartD_Prescriber_PUF_NPI_16/PartD_Prescriber_PUF_NPI_16.txt", stringsAsFactors=FALSE)
 Prescriber_16 <- Prescriber_16[Prescriber_16$specialty_description == "Obstetrics/Gynecology",]
 
-Prescriber_17 <- read.delim("D:/muffly/data/Originals/PartD_Prescriber_PUF_NPI_17/PartD_Prescriber_PUF_NPI_17.txt", stringsAsFactors=FALSE)
+Prescriber_17 <- read.delim("~/Dropbox/Pharma_Influence/data/Medicare Part D data/PartD_Prescriber_PUF_NPI_17/PartD_Prescriber_PUF_NPI_17.txt", stringsAsFactors=FALSE)
 Prescriber_17 <- Prescriber_17[Prescriber_17$specialty_description == "Obstetrics & Gynecology",]
 
 Prescriber <- merge(Prescriber_13, Prescriber_14,all = TRUE)
@@ -112,10 +130,10 @@ OP_Name <- sqldf('select OP.Physician_Profile_ID, OP.Physician_Last_Name, OP.Phy
 
 OP_Name <- OP_Name[!is.na(OP_Name$Physician_Profile_ID),]
 
-write.csv(OP_Name, "D:/muffly/data/OP_Name.csv",row.names = FALSE)
-write.csv(Prescriber_Name, "D:/muffly/data/Prescriber_Name.csv",row.names = FALSE)
-write.csv(OP,"d:/muffly/data/OP.csv",row.names = FALSE)
-write.csv(Prescriber, "D:/muffly/data/Prescriber.csv",row.names = FALSE)
+write.csv(OP_Name, "~/Dropbox/Pharma_Influence/data/OP_Name.csv",row.names = FALSE)
+write.csv(Prescriber_Name, "~/Dropbox/Pharma_Influence/data/Prescriber_Name.csv",row.names = FALSE)
+write.csv(OP,"~/Dropbox/Pharma_Influence/data/OP.csv",row.names = FALSE)
+write.csv(Prescriber, "~/Dropbox/Pharma_Influence/data/Prescriber.csv",row.names = FALSE)
 
 #*********************************************************************************************
 # start temp
@@ -341,12 +359,17 @@ Prescriber_Name_UnMatched <- Prescriber_Name[Prescriber_Name$PPI == "",]
 
 #OP_Name <- sqldf('select OP_Name.*, match_Dup.MNPI from OP_Name left outer join match_Dup on OP_Name')
 
-write.csv(unmatch_Dup,"D:/muffly/data/unmatch_dups.csv",row.names = FALSE)
-write.csv(OP_Name_Matched, "D:/muffly/data/OP_Name_Matched.csv",row.names = FALSE)
-write.csv(OP_Name_UnMatched, "D:/muffly/data/OP_Name_UnMatched.csv",row.names = FALSE)
+write.csv(unmatch_Dup,"~/Dropbox/Pharma_Influence/data/unmatch_dups.csv",row.names = FALSE)
+write.csv(OP_Name_Matched, "~/Dropbox/Pharma_Influence/data/OP_Name_Matched.csv",row.names = FALSE)
+write.csv(OP_Name_UnMatched, "~/Dropbox/Pharma_Influence/data/OP_Name_UnMatched.csv",row.names = FALSE) #See Julie Lemoine as an example.  
 
-write.csv(Prescriber_Name_Matched, "D:/muffly/data/Prescriber_Name_Matched.csv",row.names = FALSE)
-write.csv(Prescriber_Name_UnMatched, "D:/muffly/data/Prescriber_Name_UnMatched.csv",row.names = FALSE)
+write.csv(Prescriber_Name_Matched, "~/Dropbox/Pharma_Influence/data/Prescriber_Name_Matched.csv",row.names = FALSE)  #Unmatched because they had no open payments???
 
+write.csv(Prescriber_Name_UnMatched, "~/Dropbox/Pharma_Influence/data/Prescriber_Name_UnMatched.csv",row.names = FALSE)  #People who are unmatched in the prescriber_name_unmatched.csv file means they prescribed money but did not have any conflicts of interest in Open Payments?  
+
+#Value is the amount of money that matched to data or did not match?
 value <- sqldf('select OP.Physician_Profile_ID, sum(OP.Total_Amount_of_Payment_USDollars) as Total from OP group by OP.Physician_Profile_ID')
-write.csv(value, "D:/muffly/data/value.csv")
+
+
+write.csv(value, "~/Dropbox/Pharma_Influence/data/value.csv")
+
