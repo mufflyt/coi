7d1e43715aaa044a79df67a2912e06cda6ddb37b
Tyler Muffly <tyler.muffly@dhha.org>
2020-05-01 14:49:23 -0600

    Changes to data prep.

--- Code/0_Data_Prep.R
+++ Code/0_Data_Prep.R
@@ -11,6 +11,7 @@
 # install.packages('reshape')
 # install.packages('reshape2')
 # install.packages("tidyverse")
+# install.packages("readxl")
 
 # Loading
 library("sqldf")
@@ -21,19 +22,21 @@ library("Hmisc")
 library('reshape')
 library('reshape2')
 library("tidyverse")
+library("readxl")
 
 #Prep to PCND to get it managable and save as a new CSV for use
 PCND <- readr::read_csv("/Volumes/Projects/Pharma_Influence/Data/Physician_Compare/Physician_Compare_National_Downloadable_File.csv") %>% 
-  dplyr::select(NPI, lst_nm, frst_nm, mid_nm, gndr, Med_sch, Grd_yr, pri_spec, sec_spec_1, sec_spec_2, sec_spec_3, sec_spec_4, adr_ln_1, cty, st, zip) %>%
-  rename(State = st, City = cty, Zip.Code = zip, First.Name = frst_nm, Last.Name = lst_nm, Middle.Name = mid_nm, Primary.specialty = pri_spec, Secondary.speciality.1 = sec_spec_1, Secondary.speciality.2 = sec_spec_2, Secondary.speciality.3 = sec_spec_3, Secondary.speciality.4 = sec_spec_4, Line.1.Street.Address = adr_ln_1, Gender = gndr, Medical.School.Name = Med_sch, Graduation.year = Grd_yr)
+  dplyr::select(NPI, lst_nm, frst_nm, mid_nm, gndr, Med_sch, Grd_yr, pri_spec, sec_spec_1, sec_spec_2, sec_spec_3, sec_spec_4, adr_ln_1, cty, st, zip) 
+  #rename(State = st, City = cty, Zip.Code = zip, First.Name = frst_nm, Last.Name = lst_nm, Middle.Name = mid_nm, Primary.specialty = pri_spec, Secondary.speciality.1 = sec_spec_1, Secondary.speciality.2 = sec_spec_2, Secondary.speciality.3 = sec_spec_3, Secondary.speciality.4 = sec_spec_4, Line.1.Street.Address = adr_ln_1, Gender = gndr, Medical.School.Name = Med_sch, Graduation.year = Grd_yr)
 readr::write_csv(PCND, "/Volumes/Projects/Pharma_Influence/Data/Physician_Compare/Physician_Compare_National_Downloadable_File2.csv")
+
 
 #Prep NPPES to get it managable and save as new csv for use
 NPPES <- read.csv("/Volumes/Projects/Pharma_Influence/Data/NPPES_Data_Dissemination_April_2020/npidata_pfile_20050523-20200412.csv", stringsAsFactors = FALSE) %>%
 dplyr::filter(Entity.Type.Code == "1") %>%  #Remove all hospitals and nursing homes
 dplyr::filter(Provider.Business.Practice.Location.Address.State.Name %nin% c("ZZ", "AS", "FM", "GU", "MH", "MP", "PW", "VI")) %>% #Take out the places are not in the United States
-  dplyr::select(c("NPI", starts_with("Provider")) %>% #Then select out the columns that you want
-  dplyr::select(c(-contains("License.Number", -"Provider.Enumeration.Date", -contains(".Second.Line."), -ends_with(".Number"))))
+  dplyr::select(c("NPI", starts_with("Provider"))) %>% #Then select out the columns that you want
+  dplyr::select(c(-contains("License.Number"), -"Provider.Enumeration.Date", -contains(".Second.Line."), -ends_with(".Number")))
 
 readr::write_csv(NPPES, "/Volumes/Projects/Pharma_Influence/Data/NPPES_Data_Dissemination_April_2020/npidata_pfile_20050523-20200412_2.csv")
 
@@ -55,4 +58,10 @@ GOBA_unique <- read.csv("/Volumes/Projects/Pharma_Influence/Data/GOBA_unique.csv
 
 write_csv(GOBA_unique, "/Volumes/Projects/Pharma_Influence/Data/GOBA_unique_2.csv")
 
+#Rural zip Codes Imported
+rural_zip_codes <- readxl::read_xlsx("/Volumes/Projects/Pharma_Influence/Data/Rurality/nonmetrocountiesandcts2016.xlsx") %>%
+  dplyr::select(-CT, -`RUCA 2010`, -Memo) %>%
+  dplyr::rename(State = ST, Zip_Code = `CTY FIPS`) %>%
+  distinct(Zip_Code, .keep_all = TRUE) %>%
+  mutate(Rural_Zip_codes = "Rural Zip codes")
 
