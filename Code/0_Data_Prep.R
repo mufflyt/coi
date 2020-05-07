2b8c8f6e5c67494d1070191678df588f953cb4b2
Tyler Muffly <tyler.muffly@dhha.org>
2020-05-07 13:55:28 -0600

    Great.

--- Code/0_Data_Prep.R
+++ Code/0_Data_Prep.R
@@ -24,26 +24,40 @@ library('reshape2')
 library("tidyverse")
 library("readxl")
 library("RSocrata")
+library("exploratory")
+library("tidyr")
 
 #Prep to PCND to get it managable and save as a new CSV for use ----
 #https://data.medicare.gov/Physician-Compare/Physician-Compare-National-Downloadable-File/mj5m-pzi6
 #The API is nice because you do not have to store a huge file or take the time to download it.  
-PCND <- read.socrata(
-    "https://data.medicare.gov/resource/mj5m-pzi6.json",
-    app_token = "vZUBqP0g0i4Lr3vXOqNxCjzyL",
-    email     = "tyler.muffly@dhha.org",
-    password  = "Balance1"
-  ) %>%
-  distinct(npi, .keep_all = TRUE) %>%
-  tidyr::drop_na(npi) %>%
-  dplyr::select(npi, lst_nm, frst_nm, mid_nm, cred, gndr, med_sch, grd_yr, pri_spec, sec_spec_1, sec_spec_2, sec_spec_3, sec_spec_4, adr_ln_1, cty, st, zip) %>%
-  dplyr::rename(State = st, City = cty, Zip.Code = zip, First.Name = frst_nm, Last.Name = lst_nm, Middle.Name = mid_nm, Primary.specialty = pri_spec, Secondary.specialty.1 = sec_spec_1, Secondary.specialty.2 = sec_spec_2, Secondary.specialty.3 = sec_spec_3, Secondary.specialty.4 = sec_spec_4, Line.1.Street.Address = adr_ln_1, Gender = gndr, Medical.School.Name = med_sch, Graduation.year = grd_yr) %>%
+PCND1 <- 
+  read_csv("/Volumes/Projects/Pharma_Influence/Data/Physician_Compare/Physician_Compare_National_Downloadable_File.csv") %>%
+  # read.socrata(
+  #   "https://data.medicare.gov/resource/mj5m-pzi6.json",
+  #   app_token = "vZUBqP0g0i4Lr3vXOqNxCjzyL",
+  #   email     = "tyler.muffly@dhha.org",
+  #   password  = "Balance1"
+  # ) %>%
+  distinct(NPI, .keep_all = TRUE) %>%
+  tidyr::drop_na(NPI) %>%
+  dplyr::select(NPI, lst_nm, frst_nm, mid_nm, Cred, gndr, Med_sch, Grd_yr, pri_spec, sec_spec_1, sec_spec_2, sec_spec_3, sec_spec_4, adr_ln_1, cty, st, zip) %>%
+  dplyr::rename(State = st, City = cty, Zip.Code = zip, First.Name = frst_nm, Last.Name = lst_nm, Middle.Name = mid_nm, Primary.specialty = pri_spec, Secondary.specialty.1 = sec_spec_1, Secondary.specialty.2 = sec_spec_2, Secondary.specialty.3 = sec_spec_3, Secondary.specialty.4 = sec_spec_4, Line.1.Street.Address = adr_ln_1, Gender = gndr, Medical.School.Name = Med_sch, Graduation.year = Grd_yr) %>%
   dplyr::filter(State %nin% c("AP","AE", "AS", "FM", "GU", "MH","MP", "PR","PW","UM","VI", "ZZ")) %>%
-  dplyr::filter(Primary.specialty %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY"))  | (Secondary.specialty.1 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) | (Secondary.specialty.2 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) | (Secondary.specialty.3 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) | (Secondary.specialty.4 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) %>%
-  substr(Zip.Code,1,5) %>%
-  arrange (Last.Name) %>%
-  tolower(c(Last.Name, First.Name, Middle.Name, Line.1.Street.Address, City, State)) %>%
-  readr::write_csv("/Volumes/Projects/Pharma_Influence/Data/Physician_Compare/Physician_Compare_National_Downloadable_File2.csv")
+  dplyr::filter(Primary.specialty %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY"))  %>%
+# | (Secondary.specialty.1 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) | (Secondary.specialty.2 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) | (Secondary.specialty.3 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) | (Secondary.specialty.4 %in% c("GYNECOLOGICAL ONCOLOGY", "OBSTETRICS/GYNECOLOGY")) %>%
+  #substr(x = Zip.Code, start =1, stop =5) %>%
+  dplyr::arrange (Last.Name) %>%
+  #tolower(c(Last.Name, First.Name, Middle.Name, Line.1.Street.Address, City, State)) %>%
+  distinct(NPI, .keep_all = TRUE) %>%
+  dplyr::mutate_at(vars(Last.Name, First.Name, Middle.Name, Line.1.Street.Address), funs(str_to_title)) %>%
+  dplyr::mutate(Middle.Name = impute_na(Middle.Name, type = "value", val = "")) %>%
+  dplyr::mutate_at(vars(Last.Name, First.Name, Middle.Name), funs(str_clean)) %>%
+  tidyr::unite(full.name.1, First.Name, Middle.Name, Last.Name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  tidyr::unite(full.name.2, First.Name, Middle.Name, Last.Name, State, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  tidyr::unite(full.name.3, Line.1.Street.Address, State, Last.Name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  dplyr::mutate(Line.1.Street.Address = str_to_title(Line.1.Street.Address)) %>%
+  tidyr::unite(full.name.5, Line.1.Street.Address, State, Last.Name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  readr::write_csv("/Volumes/Projects/Pharma_Influence/Data/Physician_Compare/Physician_Compare_National_Downloadable_File2.csv")  #fucking finally
 
 
 taxonomy_codes <- c("207V00000X", #Blank, general obgyn
@@ -59,46 +73,31 @@ taxonomy_codes <- c("207V00000X", #Blank, general obgyn
                     "17400000X", "390200000X", "208D0000X") 
 
 #NPPES ----
-#Prep downloaded file NPPES to get it managable and save as new csv for use
-NPPES <- read.csv("/Volumes/Projects/Pharma_Influence/Data/NPPES_Data_Dissemination_April_2020/npidata_pfile_20050523-20200412.csv", stringsAsFactors = FALSE) %>%
+NPPES <- read.csv("/Volumes/Projects/Pharma_Influence/Data/NPPES_Data_Dissemination_April_2020/npidata_pfile_20050523-20200412.csv") #This takes 20 minutes to load.  Christ have mercy!  There is no RSocrata API so I downloaded the file.  
+  Sys.time()
+
+NPPES1 <- NPPES %>%
 dplyr::filter(Entity.Type.Code == "1") %>%  #Remove all hospitals and nursing homes
 dplyr::filter(Provider.Business.Practice.Location.Address.State.Name %nin% c("ZZ", "AS", "FM", "GU", "MH", "MP", "PW", "VI")) %>% #Take out the places are not in the United States
   distinct(NPI, .keep_all = TRUE) %>%
   mutate(Provider.Credential.Text = str_remove_all(Provider.Credential.Text, "[[:punct:]]+")) %>%
-  mutate(Provider.Credential.Text = str_clean(Provider.Credential.Text)) %>%
-  select(-Provider.Organization.Name..Legal.Business.Name., -Provider.Other.Organization.Name, -Provider.Other.Organization.Name.Type.Code, -Provider.Other.Name.Suffix.Text) %>%
+  mutate(Provider.Credential.Text = exploratory::str_clean(Provider.Credential.Text)) %>%
   filter(Provider.Business.Practice.Location.Address.Country.Code..If.outside.U.S.. == "US" & Provider.Business.Mailing.Address.Country.Code..If.outside.U.S.. == "US") %>%
   mutate_at(vars(Provider.Last.Name..Legal.Name., Provider.First.Name, Provider.Middle.Name, Provider.Other.Last.Name, Provider.Other.First.Name, Provider.Other.Middle.Name), funs(str_to_title)) %>%
-  drop_na(Provider.Last.Name..Legal.Name., Provider.First.Name) %>%
-  mutate(Provider.Middle.Name = impute_na(Provider.Middle.Name, type = "value", val = ""), Provider.Middle.Name = str_clean(Provider.Middle.Name)) %>%
+  tidyr::drop_na(Provider.Last.Name..Legal.Name., Provider.First.Name) %>%
+  mutate(Provider.Middle.Name = impute_na(Provider.Middle.Name, type = "value", val = ""), Provider.Middle.Name = exploratory::str_clean(Provider.Middle.Name)) %>%
   mutate(Provider.Business.Mailing.Address.State.Name = str_remove_all(Provider.Business.Mailing.Address.State.Name, "[[:punct:]]+")) %>%
   filter(Provider.Business.Mailing.Address.State.Name %in% c("AK", "AR", "AL", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NV", "NY", "OH", "NM", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV")) %>%
   filter(Provider.Business.Practice.Location.Address.State.Name %in% c("AK", "AR", "AL", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NV", "NY", "OH", "NM", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV")) %>%
   distinct(NPI, .keep_all = TRUE) %>%
   filter(Provider.Credential.Text %in% c("MD", "DO")) %>%
-  filter(Healthcare.Provider.Taxonomy.Code_1 %in% taxonomy_codes
-         | Healthcare.Provider.Taxonomy.Code_2 %in% taxonomy_codes
-         | Healthcare.Provider.Taxonomy.Code_3 %in% taxonomy_codes
-         | Healthcare.Provider.Taxonomy.Code_4 %in% taxonomy_codes
-         | Healthcare.Provider.Taxonomy.Code_5 %in% taxonomy_codes) %>%
-  distinct(NPI, .keep_all = TRUE) %>%
-  filter(Provider.Credential.Text %in% c("MD", "DO")) %>%
-  mutate(Provider.Name.Suffix.Text = exploratory::impute_na(Provider.Name.Suffix.Text, type = "value", val = ""), 
-         Provider.Other.Last.Name = exploratory::impute_na(Provider.Other.Last.Name, type = "value", val = ""), 
-         Provider.Other.First.Name = exploratory::impute_na(Provider.Other.First.Name, type = "value", val = ""), 
-         Provider.Other.Middle.Name = exploratory::impute_na(Provider.Other.Middle.Name, type = "value", val = "")) %>%
-  
-  #Creating nppes.full.names.1,2,3, plus the name&state option
+  mutate(Provider.Name.Suffix.Text = impute_na(Provider.Name.Suffix.Text, type = "value", val = ""), Provider.Other.Last.Name = impute_na(Provider.Other.Last.Name, type = "value", val = ""), Provider.Other.First.Name = impute_na(Provider.Other.First.Name, type = "value", val = ""), Provider.Other.Middle.Name = impute_na(Provider.Other.Middle.Name, type = "value", val = "")) %>%
   unite(nppes.full.name.1, Provider.First.Name, Provider.Middle.Name, Provider.Last.Name..Legal.Name., sep = " ", remove = FALSE, na.rm = FALSE) %>%
   unite(nppes.full.name.2, Provider.First.Name, Provider.Middle.Name, Provider.Last.Name..Legal.Name., Provider.Name.Suffix.Text, sep = " ", remove = FALSE, na.rm = FALSE) %>%
   unite(nppes.full.name.3, Provider.Other.First.Name, Provider.Other.Middle.Name, Provider.Other.Last.Name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
   unite(nppes.full.name.state, Provider.First.Name, Provider.Middle.Name, Provider.Last.Name..Legal.Name., Provider.Business.Mailing.Address.State.Name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
-  mutate(nppes.full.name.state = str_clean(nppes.full.name.state))
-  
-  #dplyr::select(c("NPI", starts_with("Provider"))) %>% #Then select out the columns that you want
-  #dplyr::select(c(-contains("License.Number"), -"Provider.Enumeration.Date", -contains(".Second.Line."), -ends_with(".Number")))
-
-readr::write_csv(NPPES, "/Volumes/Projects/Pharma_Influence/Data/NPPES_Data_Dissemination_April_2020/npidata_pfile_20050523-20200412_2.csv")
+  mutate(nppes.full.name.state = str_clean(nppes.full.name.state))%>%
+  readr::write_csv("/Volumes/Projects/Pharma_Influence/Data/NPPES_Data_Dissemination_April_2020/npidata_pfile_20050523-20200412_2.csv")
 
 
 #GOBA ----
@@ -147,32 +146,36 @@ taxonomy_codes <- c("207V00000X", #Blank, general obgyn
 
 #https://dev.socrata.com/foundry/openpaymentsdata.cms.gov/tr8n-5p4d
 #Takes about 10 minutes
-OP_Summary <- read.socrata(
-  "https://openpaymentsdata.cms.gov/resource/tr8n-5p4d.json",
-  app_token = "vZUBqP0g0i4Lr3vXOqNxCjzyL",
-  email     = "tyler.muffly@dhha.org",
-  password  = "Balance1"
-) %>%
+
+
+
+OP_Summary <- exploratory::read_delim_file("/Volumes/Projects/Pharma_Influence/Data/Open_Payments/OP_PH_PRFL_SPLMTL_P01172020_2.csv" , ",", quote = "\"", skip = 0 , col_names = TRUE , na = c('','NA') , locale=readr::locale(encoding = "UTF-8", decimal_mark = ".", grouping_mark = "," ), trim_ws = TRUE , progress = FALSE) %>%
+#   read.socrata(
+#   "https://openpaymentsdata.cms.gov/resource/tr8n-5p4d.json",
+#   app_token = "vZUBqP0g0i4Lr3vXOqNxCjzyL",
+#   email     = "tyler.muffly@dhha.org",
+#   password  = "Balance1"
+# ) %>%
   filter(physician_profile_country_name=="UNITED STATES") %>%
   filter(physician_profile_ops_taxonomy_1 %in% taxonomy_codes
          | physician_profile_ops_taxonomy_2 %in% taxonomy_codes
          | physician_profile_ops_taxonomy_3 %in% taxonomy_codes
          | physician_profile_ops_taxonomy_4 %in% taxonomy_codes
          | physician_profile_ops_taxonomy_5 %in% taxonomy_codes) %>%
-  separate(physician_profile_zipcode, into = c("physician_profile_zipcode_1", "physician_profile_zipcode_2"), sep = "\\s*\\-\\s*", convert = TRUE) %>%
-  select(-physician_profile_zipcode_2) %>%
-  mutate(physician_profile_zipcode_1 = as.character(physician_profile_zipcode_1)) %>%
-  mutate(physician_profile_zipcode_1 = str_pad(physician_profile_zipcode_1, pad="0", side="left", width=5)) %>%
-  filter(!is.na(physician_profile_zipcode_1)) %>%
+  #separate(physician_profile_zipcode, into = c("physician_profile_zipcode_1", "physician_profile_zipcode_2"), sep = "\\s*\\-\\s*", convert = TRUE) %>%
+  #select(-physician_profile_zipcode_2) %>%
+  #mutate(physician_profile_zipcode_1 = as.character(physician_profile_zipcode_1)) %>%
+  #mutate(physician_profile_zipcode_1 = str_pad(physician_profile_zipcode_1, pad="0", side="left", width=5)) %>%
+  #filter(!is.na(physician_profile_zipcode_1)) %>%
   mutate_at(vars(physician_profile_first_name, physician_profile_last_name, physician_profile_alternate_first_name, physician_profile_alternate_middle_name, physician_profile_alternate_last_name, physician_profile_middle_name), funs(str_to_title)) %>%
   mutate(physician_profile_middle_name = exploratory::impute_na(physician_profile_middle_name, type = "value", val = "")) %>%
   distinct(physician_profile_id, .keep_all = TRUE) %>%
-  select(-physician_profile_address_line_2, -physician_profile_province_name) %>% 
-  unite(full.name.1, physician_profile_first_name, physician_profile_middle_name, physician_profile_last_name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  #select(-physician_profile_address_line_2, -physician_profile_province_name) %>% 
+  unite(OP.full.name.1, physician_profile_first_name, physician_profile_middle_name, physician_profile_last_name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
   mutate(physician_profile_suffix = impute_na(physician_profile_suffix, type = "value", val = ""), physician_profile_alternate_suffix = impute_na(physician_profile_alternate_suffix, type = "value", val = ""), physician_profile_alternate_middle_name = impute_na(physician_profile_alternate_middle_name, type = "value", val = ""), physician_profile_alternate_last_name = impute_na(physician_profile_alternate_last_name, type = "value", val = "")) %>%
-  unite(full.name.2, physician_profile_first_name, physician_profile_middle_name, physician_profile_last_name, physician_profile_suffix, sep = " ", remove = FALSE, na.rm = FALSE) %>%
-  unite(full.name.3, physician_profile_alternate_first_name, physician_profile_alternate_middle_name, physician_profile_alternate_last_name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
-  unite(full.name.4, physician_profile_alternate_first_name, physician_profile_alternate_middle_name, physician_profile_alternate_last_name, physician_profile_alternate_suffix, sep = " ", remove = FALSE, na.rm = FALSE)
+  unite(OP.full.name.2, physician_profile_first_name, physician_profile_middle_name, physician_profile_last_name, physician_profile_suffix, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  unite(OP.full.name.3, physician_profile_alternate_first_name, physician_profile_alternate_middle_name, physician_profile_alternate_last_name, sep = " ", remove = FALSE, na.rm = FALSE) %>%
+  unite(OP.full.name.4, physician_profile_alternate_first_name, physician_profile_alternate_middle_name, physician_profile_alternate_last_name, physician_profile_alternate_suffix, sep = " ", remove = FALSE, na.rm = FALSE)
 
 write_csv(OP_Summary, "/Volumes/Projects/Pharma_Influence/Data/Open_Payments/OP_PH_PRFL_SPLMTL_P01172020_2.csv")
 
@@ -248,3 +251,4 @@ OP_ALL_Years <- bind_rows(OP2013, OP2014, OP2015, OP2016, OP2017, OP2018, id_col
 
 Sys.time()
 View(OP_ALL_Years)
+
