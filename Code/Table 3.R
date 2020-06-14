c5c2b82106dfa28f9e3ae36efb5196469944b2d7
Tyler Muffly <tyler.muffly@dhha.org>
2020-06-13 19:55:58 -0600

    T3 links changed to dropbox.

--- Code/Table 3.R
+++ Code/Table 3.R
@@ -5,14 +5,14 @@ library(reshape2)
 library(summarize)
 #load data (in case this is run independantly)
 
-PaySum <- read.csv("C:/Users/jguido/Dropbox (Personal)/Pharma_Influence/Guido_Working_file/paymentSummary.csv", stringsAsFactors=FALSE)
+PaySum <- read.csv("https://www.dropbox.com/s/sw34j1tm86kwcib/paymentSummary.csv?raw=1", stringsAsFactors=FALSE)
 
 #normalize column names, change drug names to lower case
 names(PaySum) <- c("PPI", "ManName", "ManID","TotalPay","DrugName", "DrugNDC", "Year", "DrugCount" )
 PaySum$DrugName <- tolower(PaySum$DrugName)
 
 #Load Class Data
-Payment_Class <- read.csv("C:/Users/jguido/Dropbox (Personal)/Pharma_Influence/Guido_Working_file/Payment_Class.csv", stringsAsFactors=FALSE)
+Payment_Class <- read.csv("https://www.dropbox.com/s/jy8axcdrggp60iq/Payment_Class.csv?raw=1", stringsAsFactors=FALSE)
 
 # add class info to Payments and Prescriptions
 
@@ -22,6 +22,7 @@ PaySum <- PaySum[!is.na(PaySum$Class),]
 # combine on years prior to calculating medians
 PaySum_combined <- sqldf(' select PPI, Class, Drug, sum(TotalPay) as "TotalPay" from PaySum group by PPI, Class, Drug')
 
+#Can't fun this medIQR line below - Tyler
 T3<-  (medIQR(TotalPay ~  Drug,PaySum_combined   ))
 
 write.csv(T3,"T3.csv")
